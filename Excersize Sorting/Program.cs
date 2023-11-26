42b3dbc00725ddd153b59bdfd8c5159501825cc4
returnaction <obergannv@gmail.com>
2023-11-25 22:58:27 -0500

    Add NonComparisson Sort CountingSort

--- Excersize Sorting/Program.cs
+++ Excersize Sorting/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security;
 
 namespace Excersize_Sorting
 {
@@ -22,7 +25,6 @@ namespace Excersize_Sorting
         {
 
             /// https://leetcode.com/explore/learn/card/sorting/
-            /// 
 
             //string[] words = { "we", "top", "are", "dic" };
 
@@ -31,15 +33,16 @@ namespace Excersize_Sorting
             //foreach(string w in words)
             //    Console.WriteLine(w);
 
-            int[] arr = { 1, 1, 4, 2, 1, 3 };
-            //int[] result = SelectionSort(arr);
-            //int[] result2 = BubbleSort(arr);
-            int result = HeightChecker(arr);
+            int[] array = { 5,3,2,1,4 };
+
+            CountingSort(array);
 
 
         }
 
-        // selection Sort
+                    //  Comparisson Sort
+
+        // Selection Sort
         static public int[] SelectionSort(int[] arr)
         {
             int minIndex;
@@ -54,9 +57,9 @@ namespace Excersize_Sorting
                         minIndex = j;
                 }
 
-                int temp = arr[minIndex];
-                arr[minIndex] = arr[i];
-                arr[i] = temp;
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
             }
 
             return arr;
@@ -108,7 +111,7 @@ namespace Excersize_Sorting
 
             int[] sortArr = new int[arr.Length];
             int counter = 0;
-            foreach(int num in arr)
+            foreach (int num in arr)
             {
                 sortArr[counter++] = num;
             }
@@ -167,6 +170,170 @@ namespace Excersize_Sorting
             }
 
             return res;
+        }
+
+
+        // Insertion Sort
+        static public int[] InsertionSort(int[] arr)
+        {
+            // is good for small array;
+            // is good when input is very close to being sorted;
+
+                //7, 3, 2, 5, 6, 10, 9, 8, 1 
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int curI = i;
+
+                while (curI > 0 && arr[curI - 1] > arr[curI])
+                {
+                    int temp = arr[curI];
+                    arr[curI] = arr[curI - 1];
+                    arr[curI - 1] = temp;
+                    curI--;
+                }
+            }
+
+            return arr;
+        }
+
+        static void HeapSort(int[] arr)
+        {
+            for (int i = arr.Length / 2 - 1; i >= 0; i--)
+            {
+                MaxHeapify(arr, arr.Length, i);
+            }
+
+            for (int i = arr.Length - 1; i > 0; i--)
+            {
+                int temp = arr[i];
+                arr[i] = arr[0];
+                arr[0] = temp;
+                MaxHeapify(arr, i, 0);
+            }
+        }
+
+        public static void MaxHeapify(int[] arr, int heapSize, int index)
+        {
+            int left = 2 * index + 1;
+            int right = 2 * index + 2;
+            int largest = index;
+            if (left < heapSize && arr[left] > arr[largest])
+            {
+                largest = left;
+            }
+            if (right < heapSize && arr[right] > arr[largest])
+            {
+                largest = right;
+            }
+            if (largest != index)
+            {
+                int temp = arr[index];
+                arr[index] = arr[largest];
+                arr[largest] = temp;
+                MaxHeapify(arr, heapSize, largest);
+            }
+        }
+
+                    // Non Comparisson Sort
+        // CountingSort
+        public static void CountingSort(int[] arr)
+        {
+            int MaxElement(int[] arr)
+            {
+                int maxElement = arr[0];
+
+                for(int i = 1; i < arr.Length; i++)
+                {
+                    if (arr[i] > maxElement)
+                        maxElement = arr[i];
+                }
+
+                return maxElement;
+            }
+
+            int maxElement = MaxElement(arr);
+
+            int[] occurrences = new int[maxElement + 1];
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                occurrences[arr[i]]++;
+            }
+
+            for(int i = 0, j = 0; i <= maxElement; i++)
+            {
+                while (occurrences[i] > 0)
+                {
+                    arr[j] = i;
+                    j++;
+                    occurrences[i]--;
+                }
+            }
+        }
+
+
+       
+
+    }
+
+
+    // Kastet help me
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode? next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode InsertionSortList(ListNode head)
+        { //https://leetcode.com/explore/learn/card/sorting/694/comparison-based-sorts/4485/
+            if (head == null || head.next == null)
+                return head;
+
+            ListNode temp = head;
+
+            while(temp != null)
+            {
+                ListNode next = temp.next;
+                ListNode start = head;
+                ListNode prev = head;
+
+                while(start != next)
+                {
+                    if(start.val > temp.val)
+                    {
+                        temp.next = start;
+                        if(start == head)
+                        {
+                            head = temp;
+                        }
+                        else
+                        {
+                            prev.next = temp;
+                        }
+                        while(start.next != temp)
+                        {
+                            start = start.next;
+                        }
+                        start.next = next;
+                    }
+
+                    prev = start;
+                    start = start.next;
+                }
+
+                temp = next;
+            }
+
+            return head;
         }
     }
 }
---
+++ Test/Program.cs
@@ -0,0 +1,66 @@
+﻿
+
+
+using Microsoft.VisualBasic.FileIO;
+using System.ComponentModel.Design;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 3, 5, 1, 4 };
+
+            // BubbleSort 1, 2, 3, 4, 5
+
+            BubbleSort(arr);
+        }
+
+        private static void BubbleSort(int[] arr)
+        {
+            bool swapped = true;
+
+            while (swapped)
+            {
+                swapped = false;
+
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SelectionSort
+// Time Complexity Big O(n squared);
+// Space Complexity Big O(1) 
